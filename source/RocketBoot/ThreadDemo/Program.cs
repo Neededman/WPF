87eaad053e82c341ed1aad12613c8dca5b68e303
qi.li_HSW-SS <qi.li@united-imaging.com>
2019-08-29 17:55:50 +0800

    Thread Simple

---
+++ source/RocketBoot/ThreadDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using ThreadDemo.source;
+
+namespace ThreadDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleThread simpleThread = new SimpleThread();
+            simpleThread.CreateThreadNoParam();
+            simpleThread.CreateThreadWithParam("Hello, Thread With Param");
+            int tid = simpleThread.ThreadNoParam.ManagedThreadId;
+            Console.WriteLine(tid);
+            ThreadState state = simpleThread.ThreadNoParam.ThreadState;
+            Console.WriteLine(state);
+        }
+    }
+}
---
+++ source/RocketBoot/ThreadDemo/source/SimpleThread.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadDemo.source
+{
+    public class SimpleThread
+    {
+        public Thread ThreadNoParam { get; set; }
+        public Thread ThreadWithParam { get; set; }
+        public void CreateThreadNoParam()
+        {
+            this.ThreadNoParam = new Thread(new ThreadStart(Speak));
+            this.ThreadNoParam.Start();
+        }
+
+        public void CreateThreadWithParam(object word)
+        {
+            this.ThreadWithParam = new Thread(new ParameterizedThreadStart(SpeakWithParam));
+            this.ThreadWithParam.Start(word);
+        }
+
+        public void SpeakWithParam(object word)
+        {
+            Console.WriteLine(word);
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("Hello, Thread Without Param !");
+        }
+    }
+}
